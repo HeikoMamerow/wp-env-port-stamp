699a4f4fd4c79f2db7adf7b90c15f14a6ecdcdbf
Heiko Mamerow <mail@heikomamerow.dev>
2023-04-13 02:47:11 +0200

    refactor(TypeScript): add TypeScript

--- src/cli.ts
+++ src/cli.ts
@@ -1,5 +1,5 @@
 #!/usr/bin/env node
-"use strict";
+
 const fs = require("fs");
 // const path = require('path');
 const wpEnvJson = '.wp-env.json';
@@ -7,26 +7,30 @@ const wpEnvJson = '.wp-env.json';
 // const timestampSliced = num => +(num + '').slice(-4); // Last 4 numbers from timestamp.
 const port = getPortnumber();
 const testsPort = port + 1;
-const objPorts = { 'port': port, 'testsPort': testsPort };
-let wpEnvJsonData;
+const objPorts: { [index: string]: number } = {'port': port, 'testsPort': testsPort}
+let wpEnvJsonData: { [index: string]: number }
+
 /**
  * Check if ".wp-env.json" exist.
  */
 if (fs.existsSync(wpEnvJson)) {
-    wpEnvJsonData = JSON.parse(fs.readFileSync(wpEnvJson, 'utf8'));
+    wpEnvJsonData = JSON.parse(fs.readFileSync(wpEnvJson, 'utf8'))
+
+
     // Add numbers for "port" and "testsPort".
     Object.keys(objPorts).forEach(key => {
-        wpEnvJsonData[key] = objPorts[key];
+        wpEnvJsonData[key] = objPorts[key]
     });
-}
-else {
+} else {
     // No wp-env.json exist.
     wpEnvJsonData = objPorts;
 }
+
 /**
  * Write to .wp-env.json.
  */
-fs.writeFileSync(wpEnvJson, JSON.stringify(wpEnvJsonData, null, 2));
+fs.writeFileSync(wpEnvJson, JSON.stringify(wpEnvJsonData, null, 2))
+
 /**
  * Generate port number from time stamp.
  * Function restarts if port number has unwanted values.
@@ -35,16 +39,17 @@ fs.writeFileSync(wpEnvJson, JSON.stringify(wpEnvJsonData, null, 2));
  */
 function getPortnumber() {
     // Build port numbers from timestamp.
-    const maxNumber = 65535;
+    const maxNumber = 65535
     let portnumber = parseInt(Date.now().toString().slice(-5));
     if (portnumber >= maxNumber) {
         portnumber = parseInt(portnumber.toString().slice(-4));
     }
+
     // Prevent unwanted ports.
-    const notWantedPorts = [0, 79, 80, 87, 88, 442, 443, 2999, 3000, 8079, 8080, 8442, 8443, 8887, 8888, 65535];
+    const notWantedPorts = [0, 79, 80, 87, 88, 442, 443, 2999, 3000, 8079, 8080, 8442, 8443, 8887, 8888, 65535]
     if (notWantedPorts.includes(portnumber)) {
-        getPortnumber();
+        getPortnumber()
     }
-    return portnumber;
+
+    return portnumber
 }
-//# sourceMappingURL=cli.js.map
